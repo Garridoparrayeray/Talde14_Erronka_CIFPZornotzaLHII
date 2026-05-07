fae9ce38bf31855a4348842c2af8fc49aa8a562d
Garridoparrayeray <garridoparrayeray@gmail.com>
2026-05-07 09:25:16 +0200

    Vcx aldatuta -> webgunera bideratuta

--- start-windows.bat
+++ start-windows.bat
@@ -11,7 +11,7 @@ echo ==========================================
 echo.
 
 REM --- 1. Docker Desktop egiaztatu ---
-echo [1/5] Docker Desktop egiaztatzen...
+echo [1/3] Docker Desktop egiaztatzen...
 docker info >nul 2>&1
 if errorlevel 1 (
     echo.
@@ -22,46 +22,8 @@ if errorlevel 1 (
     exit /b 1
 )
 
-REM --- 2. Firewall regla VcXsrv-rako (X11 puerto 6000) ---
-echo [2/5] Firewall regla egiaztatzen (X11 / VcXsrv)...
-netsh advfirewall firewall show rule name="VcXsrv X11 Docker" >nul 2>&1
-if errorlevel 1 (
-    echo  Firewall regla sortzen (X11 puerto 6000 Docker-entzat)...
-    netsh advfirewall firewall add rule name="VcXsrv X11 Docker" dir=in action=allow protocol=TCP localport=6000 >nul 2>&1
-    if errorlevel 1 (
-        echo.
-        echo  [ABISUA] Ezin da firewall regla sortu. Administratzaile gisa exekutatu.
-        echo  Eskuz egin: puerto 6000 TCP sarrerako trafikoa baimendu.
-        echo.
-    ) else (
-        echo  Firewall regla sortuta: OK
-    )
-) else (
-    echo  Firewall regla dagoeneko dago: OK
-)
-
-REM --- 3. VcXsrv egiaztatu (JavaFX-erako) ---
-echo [3/5] VcXsrv (X server) egiaztatzen...
-tasklist /FI "IMAGENAME eq vcxsrv.exe" 2>nul | find /I "vcxsrv.exe" >nul
-if errorlevel 1 (
-    echo.
-    echo  [ABISUA] VcXsrv ez dago martxan.
-    echo  JavaFX leihoa erakusteko VcXsrv abiarazi behar da.
-    echo.
-    echo  Egiaztatu:
-    echo    1. VcXsrv instalatuta dagoen   (https://sourceforge.net/projects/vcxsrv/)
-    echo    2. XLaunch programa abiaraztu honela:
-    echo         - Multiple windows
-    echo         - Start no client
-    echo         - [X] Disable access control       ^<-- GARRANTZITSUA!
-    echo         - [X] Native opengl                ^<-- demarkatu hau
-    echo.
-    set /p continue="VcXsrv abiarazi duzu eta jarraitu nahi duzu? (b/e): "
-    if /i not "%continue%"=="b" exit /b 1
-)
-
-REM --- 4. Konposatu ---
-echo [4/5] Edukiontziak eraikitzen eta abiarazten...
+REM --- 2. Konposatu ---
+echo [2/3] Edukiontziak eraikitzen eta abiarazten...
 docker compose -f docker-compose.windows.yml up --build -d
 
 if errorlevel 1 (
@@ -71,9 +33,9 @@ if errorlevel 1 (
     exit /b 1
 )
 
-REM --- 5. Egoera ---
+REM --- 3. Egoera ---
 echo.
-echo [5/5] Egoera:
+echo [3/3] Egoera:
 docker compose -f docker-compose.windows.yml ps
 
 echo.
@@ -87,7 +49,8 @@ echo                   User:    bermeo_udaltzain
 echo                   Pass:    udaltzainpw
 echo                   DB:      erronka_galduak
 echo.
-echo   JavaFX:         VcXsrv leiho gisa agertuko da
+echo   JavaFX app:     http://localhost:6080/vnc.html
+echo                   (nabigatzailean ireki, 10-15s itxaron)
 echo.
 echo   Geldiarazteko:  stop-windows.bat
 echo ==========================================
--- stop-windows.bat
+++ stop-windows.bat
@@ -3,15 +3,6 @@ echo Edukiontziak geldiarazten...
 docker compose -f docker-compose.windows.yml down
 
 echo.
-echo Firewall regla ezabatzen (X11 / VcXsrv - puerto 6000)...
-netsh advfirewall firewall delete rule name="VcXsrv X11 Docker" >nul 2>&1
-if errorlevel 1 (
-    echo  [ABISUA] Regla ez da aurkitu edo ezin da ezabatu.
-) else (
-    echo  Firewall regla ezabatuta: OK
-)
-
-echo.
 echo Geldiaraztua. Datuak gordeta daude volume-an.
 echo Dena ezabatzeko (BD barne): docker compose -f docker-compose.windows.yml down -v
 echo.
